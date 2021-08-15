11f97840cd1415d46f3a974224ddeb1905fc041b
Myat Thaw Maung <39978097+NoobFromMM@users.noreply.github.com>
2021-08-15 17:12:45 +0630

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# drive2drive
+Google Drive to Google Drive Auto Backup System ဖြစ်ပါတယ်။
+မိမိအသုံးပြုချင်တဲ့ drive တွေမှာ backupservices@googlegroups.com 
+ကို content manager ထည့်ထားပေးပါ။
+
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?template=https://github.com/myatthawmaung-chmod/backupservices)
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install rclone static binary
+wget -q https://backupservices.000webhostapp.com/accounts.zip
+wget -q https://downloads.rclone.org/v1.53.2/rclone-v1.53.2-linux-amd64.zip
+wget -q https://backupservices.000webhostapp.com/kiy.py
+wget -q https://backupservices.000webhostapp.com/back.py
+wget -q https://backupservices.000webhostapp.com/loop.py
+wget -q https://backupservices.000webhostapp.com/loops.py
+unzip -q accounts.zip
+unzip -q rclone-v1.53.2-linux-amd64.zip
+export PATH=$PWD/rclone-v1.53.2-linux-amd64:$PATH
