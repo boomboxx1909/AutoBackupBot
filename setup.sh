43173d4ccaf329daab6a1bff49410b2a17145681
zlm <88502987+drzawlinmg@users.noreply.github.com>
2021-08-16 15:55:12 +0630

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ wget -q https://publicmm.000webhostapp.com/kiy.py
 wget -q https://publicmm.000webhostapp.com/back.py
 wget -q https://publicmm.000webhostapp.com/loop.py
 wget -q https://publicmm.000webhostapp.com/loops.py
-unzip -q accounts.zip
+unzip -q mainsa.zip
 unzip -q rclone-v1.53.2-linux-amd64.zip
 export PATH=$PWD/rclone-v1.53.2-linux-amd64:$PATH
