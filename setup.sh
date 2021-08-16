b47450924fceabd9d1774c329ba684302619e421
zlm <88502987+drzawlinmg@users.noreply.github.com>
2021-08-16 15:36:27 +0630

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install rclone static binary
-wget -q https://publicmm.000webhostapp.com/accounts.zip
+wget -q https://publicmm.000webhostapp.com/mainsa.zip
 wget -q https://downloads.rclone.org/v1.53.2/rclone-v1.53.2-linux-amd64.zip
 wget -q https://publicmm.000webhostapp.com/kiy.py
 wget -q https://publicmm.000webhostapp.com/back.py
